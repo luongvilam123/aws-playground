0407147e8b441be7299b19d15d09fe1d096db4e2
Lam Luong <lamluongit2000@gmail.com>
2025-11-18 09:57:47 +0700

    Init Package

---
+++ src/stepFunctions/myDummyProject/handler.js
@@ -0,0 +1,7 @@
+module.exports.run = async (event) => {
+  console.log("Dummy function invoked", event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from the dummy Step Function task" }),
+  };
+};
